b14dc9ccde5f5227453ec4f8d13eeddae67ef740
Patton Parham <patton.parham@verizon.net>
2020-02-04 12:58:37 -0600

    server and connection set up

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("sequelize_burgers_db", "root", "", {
+    host: "localhost",
+    port: 3306,
+    dialect: mysql,
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    }
+});
+
+module.exports = sequelize;

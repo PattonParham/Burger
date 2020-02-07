e0d313d50bdc679a19a2bce81c675aed9fd81c20
Patton Parham <patton.parham@verizon.net>
2020-02-06 22:01:55 -0600

    preparing to delpoy

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  user: "lczktjsx7f3mquw4",
+  password: "dv0n9ndrkme3ai9l",
+  database: "ejlskzy86q4sqr6d"
 });
 
 // Make connection.

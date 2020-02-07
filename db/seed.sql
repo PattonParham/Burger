34b5a5a044d9d0594eae68601d20b320d963cb90
Patton Parham <patton.parham@verizon.net>
2020-02-07 07:48:01 -0600

    needed to fix seed.sql file. Changed INSERT INTO burgers (name) to (burger_name)

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers (name) VALUES ('Black Bean Burger');
-INSERT INTO burgers (name) VALUES ('Cow KIller');
-INSERT INTO burgers (name) VALUES ('Beyond Burger');
-INSERT INTO burgers (name) VALUES ('Beet Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black Bean Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cow Killer');
+INSERT INTO burgers (burger_name) VALUES ('Beyond Burger');
+INSERT INTO burgers (burger_name) VALUES ('Beet Burger');
